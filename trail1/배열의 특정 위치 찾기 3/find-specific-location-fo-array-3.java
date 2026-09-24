bb5bfb2d6e84797be9ba60a1c38b4cef1d5c7e21
tuioe <vlsxm63@gmail.com>
2026-09-24 19:26:08 +0900

    [Passed] 배열의 특정 위치 찾기 3 (121ms, 10MB)

--- trail1/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ trail1/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -6,18 +6,15 @@ public class Main {
         String b[] = scanner.nextLine().split(" ");
         int[] array = new int[b.length];
         int max = array.length;
-        int index = 0;
-        boolean a = true;
         
         for(int i = 0; i < max; i++){
             array[i] = Integer.parseInt(b[i]);
         }
         for(int i = 0; i < max; i++) {
-            if(array[i] == 0 && a){
-                index = i;
-                a = false;
+            if(array[i] == 0){
+                System.out.println(array[i-1] + array[i-2] + array[i-3]);
+                break;
             }
         }
-        System.out.println(array[index-1] + array[index-2] + array[index-3]);
     }
 }
