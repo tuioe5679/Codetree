645f70f5a68909aa9a2ea9ba08c33d1556d33970
tuioe <vlsxm63@gmail.com>
2026-09-25 19:56:21 +0900

    [Passed] 특정 위치의 문자 2 (133ms, 10MB)

---
+++ trail1/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input[] = scanner.nextLine().split(" ");
+        System.out.println(input[1] + " " + input[4] + " " + input[7]);
+    }
+}
